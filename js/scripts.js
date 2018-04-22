aa0bbd89052178383a7848a28a8f7f5cf83c0324
unknown <swm1000@hotmail.com>
2018-04-21 22:33:27 -0400

    init fxns for slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
 let sliders = document.querySelectorAll(".slider");
 let navleft = document.querySelector("#left");
-let naveright = document.querySelector("#right");
+let navright = document.querySelector("#right");
+let currslideindex = 0;
+
+//clear all images when changing slides
+function clearallimgs() {
+    for(let i = 0; i < sliders.length; i++) {
+        sliders[i].style.display = "none";
+    }
+}
+
+function initslider() {
+    clearallimgs();
+    sliders[0].style.display = "block";
+}
+
+
+/*Run functions*/
+initslider();
